7d6e84f8ca810d1dd25c6a3031058154a03662a1
matoi0317 <matoi.shibanuma@gmail.com>
2019-10-23 07:22:13 +0900

    aaa

--- pages/camera.vue
+++ pages/camera.vue
@@ -5,11 +5,11 @@
           <p v-if="cameraErr">{{ cameraErr }}</p>
       </div>
       <div>
-          <div>left_top: {{ leftTop }}</div>
-          <div>left_bottom: {{ leftBottom }}</div>
+          <!-- <div>left_top: {{ leftTop }}</div> -->
+          <!--<div>left_bottom: {{ leftBottom }}</div>-->
           <div>left_open: {{ leftOpen }}</div>
-          <div>right_top: {{ rightTop }}</div>
-          <div>right_bottom:{{ rightBottom }}</div>
+          <!--<div>right_top: {{ rightTop }}</div>-->
+          <!--<div>right_bottom:{{ rightBottom }}</div>-->
           <div>right_open: {{ rightOpen }}</div>
           <div>currnetRate: {{ currentRate }}</div>
           <div><span :style="{ color: openEyeLabel == '開' ? '#f00' : '#000' }">{{ openEyeLabel }}</span>: {{ eyeBlinkCount }}回</div>
