8017be3c0e913dc4209c5de4ac8568fe99c22f91
matoi0317 <matoi.shibanuma@gmail.com>
2020-01-18 14:40:40 +0900

    deploy2

---
+++ pages/a.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <button class="button" @click="onClick">aaaaaa</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        async onClick() {
+            try {
+                const response = await this.$axios.post('http://localhost:5000/post', {
+                    blinkCount: 1,
+                    message: "heeeeeeeeeeee"
+                })
+            } catch (err) {
+                console.log(err)
+            }
+        },
+    }
+}
+</script>
